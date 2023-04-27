59478b1ac8e3412e5ce41544bc0608d63c4b3c99
aparajit.garg <aparajit.garg@kritikalvision.ai>
2023-04-28 02:16:35 +0530

    checking commit reflecting on github profile

--- note-taking/src/components/Login/Login.js
+++ note-taking/src/components/Login/Login.js
@@ -17,20 +17,22 @@ const Login = () => {
     }
     return (
         // <div className={classes.header}>
-        <div className={classes.main}>
-            <form onSubmit={handleSubmit}>
-                <label>Username</label>
-                <input type="text" placeholder="Enter username" onChange={updateUsername} value={userName} />
-                <label>Password</label>
-                {/* <input type="password" placeholder="Enter password" onKeyDown={(e) => setPassword(e.target.value)}></input> */}
-                <button type="submit"> {signIn ? "Sign In" : "Sign Up" }</button>
-            </form>
-            {/* <div className={classes.signUp}>
-                <p>Don't have an account?</p>
-                <button>Sign Up</button>
+        <>
+            <div className={classes.main}>
+                <form onSubmit={handleSubmit}>
+                    <label>Username</label>
+                    <input type="text" placeholder="Enter username" onChange={updateUsername} value={userName} />
+                    <label>Password</label>
+                    {/* <input type="password" placeholder="Enter password" onKeyDown={(e) => setPassword(e.target.value)}></input> */}
+                    <button type="submit"> {signIn ? "Sign In" : "Sign Up" }</button>
+                </form>
+                {/* <div className={classes.signUp}>
+                    <p>Don't have an account?</p>
+                    <button>Sign Up</button>
+                </div>
+                <p>( Demo credentials - user: demo@gmail.com | password: 123456 )</p> */}
             </div>
-            <p>( Demo credentials - user: demo@gmail.com | password: 123456 )</p> */}
-        </div>
+        </>
     );
 }
 
